cf95fa5e20e8b726e87a6959a93180a41cca6d8e
Jonathan Mayer <jtmayer@uci.edu>
2015-01-17 09:09:30 -0800

    Got the patty to lerp to the final color given a certain duration

--- Assets/Scripts/SpriteRecolor.cs
+++ Assets/Scripts/SpriteRecolor.cs
@@ -3,13 +3,22 @@ using System.Collections;
 
 public class SpriteRecolor : MonoBehaviour {
 
+	public float duration;
+	private float timeElapsed;
+
 	// Use this for initialization
 	void Start () {
-		renderer.material.color = new Color (228.0f/255.0f, 114.0f/255.0f, 128.0f/255.0f, 0.0f);
+		timeElapsed = 0.0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		//renderer.material.color = new Color (renderer.material.color.r - (228.0f - 153.0f) / 255.0f, renderer.material.color.g - (114.0f - 73.0f) / 255.0f, renderer.material.color.b - 128 / 255.0f, 0.0f);
+		if (timeElapsed < duration) {
+			timeElapsed += Time.deltaTime;
+		}
+
+		renderer.material.color = Color.Lerp (new Color (228.0f / 255.0f, 114.0f / 255.0f, 128.0f / 255.0f, 1.0f),
+		                                      new Color (153.0f / 255.0f, 73.0f / 255.0f, 0.0f, 1.0f), 
+		                                      timeElapsed / duration);
 	}
 }
